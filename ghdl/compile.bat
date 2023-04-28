413f69964b5fb7e2305d9515bb0536d183052c90
Simon Burkhardt <simon@simonmartin.ch>
2023-04-28 14:40:31 +0200

    used AXI Lite template, sim working

--- ghdl/compile.bat
+++ ghdl/compile.bat
@@ -9,13 +9,16 @@ del /Q *.cf &
 
 :: analyze
 ghdl -a pattern_detector.vhd
-ghdl -a tb_pattern_trig.vhd
+ghdl -a axis_latency_counter.vhd
+ghdl -a tb_myip.vhd
+
 :: elaborate
 ghdl -e pattern_detector
-ghdl -e tb_pattern_trig
+ghdl -e axis_latency_counter
+ghdl -e tb_myip
 :: run
-ghdl -r tb_pattern_trig --vcd=pattern_wave.vcd --stop-time=1us
-gtkwave pattern_wave.vcd pattern_waveform.gtkw
+ghdl -r tb_myip --vcd=wave.vcd --stop-time=1us
+gtkwave wave.vcd waveform.gtkw
 
 :: delete
 del /Q *.o &
